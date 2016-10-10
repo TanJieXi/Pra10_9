475cdd879297de9fe53e7b26095ac7d859b75c5b
Administrator <229008199@qq.com>
2016-10-10 15:19:15 +0800

    nihao

--- app/src/main/java/com/pra10_9/TwoActivity.java
+++ app/src/main/java/com/pra10_9/TwoActivity.java
@@ -4,11 +4,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-import java.io.File;
-
-import top.zibin.luban.Luban;
-import top.zibin.luban.OnCompressListener;
-
 public class TwoActivity extends AppCompatActivity {
 
     @Override
@@ -22,26 +17,6 @@ public class TwoActivity extends AppCompatActivity {
     }
 
     public void btnO(View view) {
-        Luban.get(this)
-                //传人要压缩的图片
-                .putGear(Luban.THIRD_GEAR)      //设定压缩档次，默认三挡
-                .setCompressListener(new OnCompressListener() { //设置回调
 
-
-
-                    @Override
-                    public void onStart() {
-                        // TODO 压缩开始前调用，可以在方法内启动 loading UI
-                    }
-                    @Override
-                    public void onSuccess(File file) {
-                        // TODO 压缩成功后调用，返回压缩后的图片文件
-                    }
-
-                    @Override
-                    public void onError(Throwable e) {
-                        // TODO 当压缩过去出现问题时调用
-                    }
-                }).launch();    //启动压缩
     }
 }
